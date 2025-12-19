7fa229d2ad01cf7391c5257dd1bd6c5f403ff304
SHuttooo <matthieuvinet04@gmail.com>
2025-12-19 10:18:30 +0100

    pilou1

--- src/web_control/setup.py
+++ src/web_control/setup.py
@@ -7,17 +7,26 @@ package_name = 'web_control'
 setup(
     name=package_name,
     version='1.0.0',
-    # --- MODIFICATION ICI : On utilise find_packages() ---
     packages=find_packages(exclude=['test']),
-    # -----------------------------------------------------
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Fichiers Launch
+        
+        # 1. Fichiers Launch
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # Fichiers Web
-        (os.path.join('share', package_name, 'web'), glob('web/*')),
+        
+        # 2. Fichiers Web (Racine)
+        # CRUCIAL : On utilise une liste en compréhension avec os.path.isfile(f)
+        # Cela dit : "Prends les fichiers dans web/, mais IGNORE les dossiers (comme navig)"
+        # C'est ça qui empêche l'erreur de build.
+        (os.path.join('share', package_name, 'web'), [f for f in glob('web/*') if os.path.isfile(f)]),
+        
+        # 3. Dossier Navig
+        # Ici on copie le contenu de ton dossier navig
+        (os.path.join('share', package_name, 'web/navig'), glob('web/navig/*')),
+        
+        # 4. Dossier Gallery (Optionnel, seulement si non vide)
         (os.path.join('share', package_name, 'web/gallery'), glob('web/gallery/*')),
     ],
     install_requires=['setuptools'],
